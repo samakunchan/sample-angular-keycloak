5fa2e17d85112ed1be93d987b316ef9feb72a4f5
samakunchan <cedric.badjah@gmail.com>
2025-08-20 11:53:38 +0200

    feat: app
    - Ajout du minimum syndical pour que les tests fonctionnent.

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,36 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { AuthenticationService } from './services/authentication.service';
+import { StorageCore } from './storage/storage.core';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('App', () => {
+  let storageSpy: jasmine.SpyObj<StorageCore>;
+  let authenticationSpy: jasmine.SpyObj<AuthenticationService>;
+  let routerSpy: jasmine.SpyObj<Router>;
   beforeEach(async () => {
+    storageSpy = jasmine.createSpyObj<StorageCore>('StorageCore', ['deleteTokens'], ['idToken']);
+    storageSpy.idToken = 'not null to trigger true some condition';
+    storageSpy.deleteTokens.and.callThrough();
+
+    authenticationSpy = jasmine.createSpyObj<AuthenticationService>('AuthenticationService', [], {
+      loaded$: of(false)
+    });
+
+    routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
+
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideHttpClientTesting(),
+        { provide: AuthenticationService, useValue: authenticationSpy },
+
+        { provide: StorageCore, useValue: storageSpy },
+
+        { provide: Router, useValue: routerSpy },
+      ]
     }).compileComponents();
   });
 
